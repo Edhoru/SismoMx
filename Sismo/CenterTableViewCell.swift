7134ad85320febc718e969887434c3bbecfecd13
Carlos <rockarlos@me.com>
2017-09-22 00:19:56 -0500

    added custom cell with data about centers

--- Sismo/CenterTableViewCell.swift
+++ Sismo/CenterTableViewCell.swift
@@ -1,7 +1,7 @@
 //
 //  CenterTableViewCell.swift
 //  Sismo
-//
+//  @rockarloz
 //  Created by Carlos Castellanos on 21/09/17.
 //  Copyright © 2017 huerdo. All rights reserved.
 //
@@ -9,16 +9,73 @@
 import UIKit
 
 class CenterTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    var mainView = UIView()
+    var placeLbl = UILabel()
+    var requestLbl = UILabel()
+    var requestData = UILabel()
+    var receiveLbl = UILabel()
+    var receiveData = UILabel()
+    var addressLbl = UILabel()
+    var addressData = UILabel()
+    var zoneData = UILabel()
+    
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.contentView.addSubview(mainView)
+        self.contentView.backgroundColor = Colors.gray
+        mainView.addSubview(placeLbl)
+        mainView.addSubview(requestLbl)
+        mainView.addSubview(requestData)
+        mainView.addSubview(receiveLbl)
+        mainView.addSubview(receiveData)
+        mainView.addSubview(addressLbl)
+        mainView.addSubview(addressData)
+        mainView.addSubview(zoneData)
+        
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
     }
+    
+    
 
+    func useData(_ center:NSDictionary) {
+    
+        mainView.layer.masksToBounds = true;
+        
+        mainView.frame = CGRect(x: 10, y: 5, width: self.frame.size.width - 20 , height: self.frame.size.height - 10 )
+        mainView.backgroundColor = UIColor.white
+        mainView.layer.cornerRadius = 5
+        
+        placeLbl.frame = UIElements.createFrame(x: 5, y: 10, w: 100, h: 100)
+        placeLbl.text = center.object(forKey: "LUGAR") as? String
+        placeLbl.sizeToFit()
+        
+        requestLbl.frame = UIElements.createFrame(x: 5, y: placeLbl.frame.size.height + placeLbl.frame.origin.y + 5, w:  self.frame.size.width - 20, h: 100)
+        requestLbl.text  = "Pidiendo"
+        requestLbl.textColor = Colors.red
+        requestLbl.sizeToFit()
+        
+        requestData.frame = UIElements.createFrame(x: 5, y: requestLbl.frame.size.height + requestLbl.frame.origin.y + 5, w:  self.frame.size.width - 20 , h: 100)
+        requestData.text = center.object(forKey: "NECESITAN") as? String
+        requestData.numberOfLines = 10
+        requestData.sizeToFit()
+        
+        
+        receiveLbl.frame = UIElements.createFrame(x: 5, y: requestData.frame.size.height + requestData.frame.origin.y + 5, w:  self.frame.size.width - 20, h: 100)
+        receiveLbl.text  = "reciben"
+        receiveLbl.textColor = Colors.red
+        receiveLbl.sizeToFit()
+        
+        receiveData.frame = UIElements.createFrame(x: 5, y: receiveLbl.frame.size.height + receiveLbl.frame.origin.y + 5, w:  self.frame.size.width - 20 , h: 100)
+        receiveData.text = center.object(forKey: "RECIBEN") as? String
+        receiveData.numberOfLines = 10
+        receiveData.sizeToFit()
+        
+    }
 }
