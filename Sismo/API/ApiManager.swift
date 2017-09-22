d3f1f284d7f24d5f19668ea8d63e9a30bcf472a5
Carlos <rockarlos@me.com>
2017-09-22 00:18:37 -0500

    add another method for get centers

--- Sismo/API/ApiManager.swift
+++ Sismo/API/ApiManager.swift
@@ -12,9 +12,9 @@ import Alamofire
 class ApiManager: NSObject {
     
     private let url = "https://acopio-api.skycatch.net/v1/"    
-    
+    private let acopios_url  = "https://script.googleusercontent.com/macros/echo?user_content_key=xRvxSqGDwWo0ansJohHHaOYPomidK_8j_oVFCWCMQmQYrySNXXhPX7yuruwUfWFvSeJaJ5FC8mDM7CN6MSdW1c95ZvWOgIShm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnOBXvm88s4kcVlUgpIxxI2X0CsK-VibkbdI1W84CsX9EGJrILcurRZJK5PMVD4CvF7xy_u9Yn2NR&lib=M_1Eu3tZF8s-WdtiFAYqlFCcXir-xNXZ_"
     static let shared = ApiManager()
-    
+    let sessionManager = Alamofire.SessionManager.default
     
     func getAcopios(_ completion: @escaping (_ response: String, _ error: Error?) -> Void) {
         
@@ -38,6 +38,24 @@ class ApiManager: NSObject {
             }
         }
     }
+    
+    func getAcopios2(_ completion: @escaping ((_ data:NSArray, _ result:Bool) -> Void)){
+    
+        sessionManager.request( acopios_url)
+            .responseJSON { response in
+                switch response.result {
+                case .success:
+                   
+                    completion(response.result.value as! NSArray, true)
+                case .failure(let error):
+                    let auxArray = NSArray ()
+                    completion(auxArray, false)
+                }
+                
+        }
+    
+    }
+    
     
     
     
---
+++ Utilities.swift
@@ -0,0 +1,9 @@
+//
+//  Utilities.swift
+//  Sismo
+//
+//  Created by Carlos Castellanos on 21/09/17.
+//  Copyright © 2017 huerdo. All rights reserved.
+//
+
+import Foundation
